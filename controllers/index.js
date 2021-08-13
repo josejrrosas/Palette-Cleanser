24755b08d6f9a5f1336678194dd4dda8cf4ce813
Jose Rosas <josejrrosas@yahoo.com>
2021-08-13 12:31:20 -0500

    created route for homepage and the handlebars for it as well

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 // const apiRoutes = require('./api');
-// const homeRoutes = require('./homeRoutes');
+const homeRoutes = require('./homeRoutes');
 const createRoutes = require('./createRoutes');
 
-// router.use('/', homeRoutes);
+router.use('/', homeRoutes);
 // router.use('/api', apiRoutes);
 router.use('/create', createRoutes);
 
