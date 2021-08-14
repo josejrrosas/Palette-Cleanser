72b6f17d0bebc51fc6ab1631e8d362f2645e0955
Gavin Reid <gavinreid94@gmail.com>
2021-08-14 13:07:39 -0500

    added login hbs and route

--- controllers/index.js
+++ controllers/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 // const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
 const createRoutes = require('./createRoutes');
+const userRoutes = require('./api/userRoutes');
 
 router.use('/', homeRoutes);
 // router.use('/api', apiRoutes);
 router.use('/create', createRoutes);
+router.use('/login', userRoutes);
 
 module.exports = router;
