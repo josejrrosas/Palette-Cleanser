13b7ced6cd24ac3fcb9bfbc0c7c2f445c66aca02
Jose Rosas <josejrrosas@yahoo.com>
2021-08-17 12:50:30 -0500

    got models done

---
+++ models/Favorite.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Favorite extends Model {}
+
+Favorite.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    color1: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    color2: {
+      type: DataTypes.STRING,
+    },
+    color3: {
+      type: DataTypes.STRING,
+    },
+    font1: {
+      type: DataTypes.STRING,
+    },
+    font2: {
+      type: DataTypes.STRING,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'favorite',
+  }
+);
+
+module.exports = Favorite;
